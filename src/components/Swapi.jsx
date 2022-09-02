806dbaa1f08ed1553f73c7ea7d2067929255cd75
Hosea Cherop <hosea.cherop@agilebiz.co.ke>
2022-09-02 03:30:15 +0300

    swapi

--- src/components/Swapi.jsx
+++ src/components/Swapi.jsx
@@ -1,24 +1,29 @@
 import React, { useState, useEffect } from "react";
-import { getChuckCategories } from "../api/api";
+import { getChuckCategories, getPeople } from "../api/api";
+import DataTable from 'react-data-table-component';
+
+const columns = [
+    {
+        name: 'Name',
+        selector: row => row.name,
+    },
+    {
+        name: 'Gender',
+        selector: row => row.gender,
+    },
+];
 
 export default function Swapi(props) {
 
-    const [categories, setCategories] = useState([]);
-
-    const [isLoading, setIsLoading] = useState(true);
+    const [people, setPeople] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [totalRows, setTotalRows] = useState(0);
+    // const [perPage, setPerPage] = useState(10);
 
     useEffect(() => {
         //load from api
         try {
-            const fetchCategories = async () => {
-                var response = await getChuckCategories();
-                setIsLoading(false)
-                if (response.status == 1) {
-                    setCategories(response.data)
-                }
-            }
-
-            fetchCategories()
+            fetchPeople(1)
         } catch (err) {
             alert('Error fetching categories: ' + err.message)
             setIsLoading(false)
@@ -26,16 +31,49 @@ export default function Swapi(props) {
 
     }, [isLoading])
 
+    const fetchPeople = async page => {
+        var response = await getPeople(page);
+        setIsLoading(true)
+        if (response.status == 1) {
+            setPeople(response.data.items)
+            setTotalRows(response.data.totalItems);
+        }
+
+        setIsLoading(false)
+    }
+
+    const handlePageChange = page => {
+        fetchPeople(page);
+    };
+
+    const tableOptions = {
+        noRowsPerPage: true
+    }
+
     return <div>
         <h3>Swapi People</h3>
 
-        {isLoading && <div>Please wait...</div>}
+        <DataTable
+            title="People"
+            columns={columns}
+            data={people}
+            progressPending={isLoading}
+            pagination
+            paginationServer
+            paginationComponentOptions={tableOptions}
+            paginationTotalRows={totalRows}
+            onChangePage={handlePageChange}
+        />
+
+        {/* {isLoading && <div>Please wait...</div>}
 
         {!isLoading && <ul>
             {categories.map(category => (
                 <h5>{category}</h5>
             ))}
-        </ul>}
+        </ul>} */}
+
+
 
     </div>
 }
